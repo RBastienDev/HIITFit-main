b77a34b0c93b0f2bb549bdd2b83cee310e8635bc
Robert B <aliceinchainsistayaway@gmail.com>
2026-05-21 08:09:51 -0400

    tabviewstyle

--- HIITFit/ContentView.swift
+++ HIITFit/ContentView.swift
@@ -4,11 +4,15 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-		VStack {
+		TabView {
 			Text("Welcome!")
+				
 			Text("Exercise 1")
+				
 			Text("Exercise 2")
+				
 		}
+		.tabViewStyle(PageTabViewStyle())
   }
 }
 
