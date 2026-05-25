8c8bb9c46d6c3cc1c73e7749c2fd4e8f46151c8e
Robert B <aliceinchainsistayaway@gmail.com>
2026-05-25 11:03:12 -0400

    Added HistoryStore (in progress)

---
+++ HIITFit/Views/HistoryView.swift
@@ -0,0 +1,45 @@
+
+
+import SwiftUI
+
+struct HistoryView: View {
+	
+		let today = Date()
+		let yesterday = Date().addingTimeInterval(-86400)
+	
+		let exercises1 = ["Squat", "Step Up", "Burpee", "Sun-Salute"]
+		let exercises2 = ["Squat", "Step Up", "Burpee"]
+		
+	
+    var body: some View {
+			VStack {
+				Text("History")
+					.font(.title)
+					.padding()
+				//exercise history
+				
+				Form{
+					Section(header:
+										Text(today.formatted(as: "MMM d"))
+						.font(.headline)) {
+							ForEach(exercises1, id: \.self){
+								exercise in Text(exercise)
+							}
+						}
+					Section(header:
+										Text(yesterday.formatted(as: "MMM d"))
+						.font(.headline)) {
+							ForEach(exercises2, id: \.self){
+								exercise in Text(exercise)
+							}
+						}
+				}
+			}
+			
+			
+    }
+}
+
+#Preview {
+    HistoryView()
+}
